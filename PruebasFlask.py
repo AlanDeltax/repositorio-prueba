662255c1eaf24d03f21e9cf00125eeafc45dd52d
AlanDeltax <dxalan10@gmail.com>
2025-04-21 04:44:51 -0600

    Adios Mundo

--- PruebasFlask.py
+++ PruebasFlask.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "Hola Mundo!"
+    return "Adios Mundo! o.o"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
+    
